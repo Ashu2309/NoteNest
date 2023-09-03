f361167ab337b4e55042eecb2712904ae0e97c12
Ashutosh <mishraashutosh2309@gmail.com>
2023-09-03 15:26:19 +0530

    updated

--- inotebook_server/routes/notes.js
+++ inotebook_server/routes/notes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const fetchuser = require("../Middleware/fetchuser");
+const fetchUser = require('../Middleware/fetchUser');
 const Notes = require("../models/Notes");
 const { body, validationResult } = require("express-validator");
 
