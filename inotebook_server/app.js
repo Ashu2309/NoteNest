428b98194e6dcf30ca35a21a65bdc0f32cc635dc
Ashutosh <mishraashutosh2309@gmail.com>
2023-09-03 14:05:47 +0530

    deploying code

--- inotebook_server/app.js
+++ inotebook_server/app.js
@@ -1,9 +1,9 @@
 require("dotenv").config();
-const connectToMongo = require("./src/db");
+// const connectToMongo = require("./src/db");
 const express = require("express");
 var cors = require("cors");
-
-connectToMongo();
+const mongoose = require("mongoose");
+// connectToMongo();
 const app = express();
 app.use(cors());
 const port = process.env.PORT || 5000;
@@ -11,10 +11,12 @@ const port = process.env.PORT || 5000;
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
--app.use(express.json());
+
+app.use(express.json());
 //available routes
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/notes", require("./routes/notes"));
+
 
 // app.get("/api/v1/login", (req, res) => {
 //   res.send("Sign in");
@@ -22,13 +24,24 @@ app.use("/api/notes", require("./routes/notes"));
 // app.get("/api/v1/signup", (req, res) => {
 //   res.send("Sign up");
 // });
-if (process.env.NODE_ENV == "production") {
-  app.use(express.static("inotebookf/build"));
-  // const path = require("path");
-  // app.get("*", (req, res) => {
-  //   res.sendFile(path.resolve(_dirname, "client", "build", "index.html"));
-  // });
-}
+// if (process.env.NODE_ENV == "production") {
+//   app.use(express.static("inotebookf/build"));
+// const path = require("path");
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(_dirname, "client", "build", "index.html"));
+// });
+// }
+
+const mongoURI = "mongodb+srv://ashuinotebook:" +
+  encodeURIComponent("ashuinotebook#2309!") +
+  "@cluster0.ximv4ax.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose.connect(
+  mongoURI,
+  () => {
+    console.log("Mongo Connected Successfully");
+  }
+);
 
 app.listen(port, () => {
   console.log(`App listening to port ${port}`);
--- inotebook_server/src/db.js
+++ inotebook_server/src/db.js
@@ -1,10 +1,12 @@
 const mongoose = require("mongoose");
-const mongoURI =
-  "mongodb+srv://ashuinotebook:ashuinotebook#2309!@cluster0.ximv4ax.mongodb.net/?retryWrites=true&w=majority";
+// const mongoURI = "mongodb+srv://ashuinotebook:" +
+//   encodeURIComponent("ashuinotebook#2309!") +
+//   "@cluster0.ximv4ax.mongodb.net/?retryWrites=true&w=majority";
+const mongoURI = "mongodb://localhost:27017";
+
 const connectToMongo = () => {
   mongoose.connect(
     mongoURI,
-    { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
     () => {
       console.log("Mongo Connected Successfully");
     }
